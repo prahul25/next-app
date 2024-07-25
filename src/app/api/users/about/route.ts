a4669f183dbcf37286b299f94b563c2fdb54f59e
prahul25 <palrahulkumar2002@gmail.com>
2024-07-25 22:56:47 +0530

    Solved minor bug in api/user/about

--- src/app/api/users/about/route.ts
+++ src/app/api/users/about/route.ts
@@ -6,11 +6,15 @@ import { NextRequest, NextResponse } from "next/server";
 connectDb()
 
 export async function GET(request: NextRequest) {
-    const userId = await getDataFromToken(request)
-    const user = await User.findOne({_id:userId}).select("-password")
-    if(!user){
-        return NextResponse.json({message:'Invalid Token'} ,{status:400})
+    try {
+        const userId = await getDataFromToken(request)
+        const user = await User.findOne({_id:userId}).select("-password")
+        if(!user){
+            return NextResponse.json({message:'Invalid Token'} ,{status:400})
+        }
+    
+        return NextResponse.json({message:'Successfully user data fetched' , data:user})
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
-
-    return NextResponse.json({message:'Successfully user data fetched' , data:user})
 }
